2f3ab0cf0c8e8924437f0b7aef899f7233002103
ourenjiang <ourenjiang0904@qq.com>
2025-06-10 17:15:47 +0800

    1. 恢复Device的单线程格式，同时在main中批量管理Device；

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,18 +2,29 @@ package org.example;
 
 import org.example.ems.device.air.custom.Device;
 
+import java.util.ArrayList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        Device device = new Device();
-        device.start();
+//        Device device = new Device();
+//        device.start();
+
+        ArrayList<Device> devices = new ArrayList<>();
+        for(int i = 0; i < 2; ++i){
+            Device device = new Device();
+            device.start();
+            devices.add(device);
+        }
 
         // 注册 JVM 关闭钩子
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            device.stop();
+            for(Device device : devices){
+                device.stop();
+            }
             System.out.println("JVM 关闭，已清理资源");
         }));
     }
--- src/main/java/org/example/ems/device/air/custom/Device.java
+++ src/main/java/org/example/ems/device/air/custom/Device.java
@@ -3,28 +3,18 @@ package org.example.ems.device.air.custom;
 import org.example.ems.device.air.DeviceBase;
 
 public class Device implements DeviceBase {
-
-
-    private int age1 = 1;
-    private int age2 = 1000;
-    private Thread workThread1;
-    private Thread workThread2;
+    private int age = 1;
+    private Thread workThread;
 
     // 为什么要将running修饰为volatile？由于它同时被子线程和外部线程访问，理论是是需要原子性的，
     // volatile是提供线程同步访问的作用吗？
-    private volatile boolean running1 = true;
-    private volatile boolean running2 = true;
+    private volatile boolean running = true;
 
     @Override
     public void start() {
-        startThread1();
-        startThread2();
-    }
+        workThread = new Thread(() -> {
 
-    private void startThread1() {
-        workThread1 = new Thread(() -> {
-
-            while (running1) {
+            while (running) {
 
                 synchronized (this) {
                     try {
@@ -36,46 +26,19 @@ public class Device implements DeviceBase {
                         break;
                     }
 
-                    System.out.println("thread run1..." + (age1++));
+                    System.out.println("thread run1..." + (age++));
                 }
             }
 
             System.out.println("线程安全终止");
         });
-        workThread1.start();
-    }
-
-    private void startThread2() {
-        workThread2 = new Thread(() -> {
-
-            while (running2) {
-
-                synchronized (this) {
-                    try {
-                        Thread.sleep(3000);
-                    } catch (InterruptedException e) {
-                        // sleep调用被外部中断信号打断时，同时该线程的中断状态会被清除；
-                        // 为了保持线程的中断状态的一致性，这里需要重新将该线程设置为中断状态，
-                        Thread.currentThread().interrupt();
-                        break;
-                    }
-
-                    System.out.println("thread run2..." + (age2++));
-                }
-            }
-
-            System.out.println("线程安全终止");
-        });
-        workThread2.start();
+        workThread.start();
     }
 
     @Override
     public void stop() {
-        running1 = false;
-        workThread1.interrupt(); // 中断阻塞中的线程
-
-        running2 = false;
-        workThread2.interrupt(); // 中断阻塞中的线程
+        running = false;
+        workThread.interrupt(); // 中断阻塞中的线程
     }
 
     @Override
