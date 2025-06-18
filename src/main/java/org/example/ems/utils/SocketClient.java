72bc6a11608766673941850eb367fb89b2bc0368
ourenjiang <ourenjiang0904@qq.com>
2025-06-18 17:15:30 +0800

    1. 常规提交;

---
+++ src/main/java/org/example/ems/utils/SocketClient.java
@@ -0,0 +1,43 @@
+package org.example.ems.utils;
+
+import java.io.*;
+import java.net.Socket;
+
+public class SocketClient {
+    Socket socket;
+    SocketClient(){
+    }
+
+    SocketClient(String host, int port) {
+        try {
+            socket = new Socket(host, port);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    void writeLine(String message) {
+        try {
+            PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
+            printWriter.println(message);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    String readLine(){
+        String result;
+        try {
+            InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            result = bufferedReader.readLine();
+            if(result == null){
+                // 对端正常关闭
+                throw new RuntimeException("对端正常关闭");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return result;
+    }
+}
