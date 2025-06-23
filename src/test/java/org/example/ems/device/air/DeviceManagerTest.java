a3c3e9db943a7cfde8eb6dcf5fdaa06c6b1b23d4
ourenjiang <ourenjiang0904@qq.com>
2025-06-23 17:42:44 +0800

    添加Guava

--- src/test/java/org/example/ems/device/air/DeviceManagerTest.java
+++ src/test/java/org/example/ems/device/air/DeviceManagerTest.java
@@ -1,8 +1,18 @@
 package org.example.ems.device.air;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
 public class DeviceManagerTest {
     public static void main(String[] args){
         DeviceManager instance = DeviceManager.getInstance();
         instance.start();
+
+        List<String> list1 = new ArrayList<>();
+        List<String> list2 = new LinkedList<>();
+        Collections.sort(list1);
+        Collections.sort(list2);
     }
 }
---
+++ src/test/java/org/example/ems/learn/MapTest.java
@@ -0,0 +1,12 @@
+package org.example.ems.learn;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapTest {
+    public static void main(String[] args) {
+        Map<String, Integer> stringIntegerMap = new HashMap<>();
+        stringIntegerMap.put("ourenjiang", 31);
+        System.out.println(stringIntegerMap);
+    }
+}
